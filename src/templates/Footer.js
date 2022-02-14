b2cbb640e35cc6f3edb334eb50b08a5f997e4d0a
Martha Dominguez <marthadominguezg@gmail.com>
2022-02-14 14:36:17 -0500

    corrección en logos del footer

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -3,10 +3,11 @@ const Footer = () => {
         <p>Martha L. Domínguez</p>
         <p>|</p>
         <a target="_blank" rel="noreferrer" className='link' href="https://www.linkedin.com/in/martha-liliana-dom%C3%ADnguez-09b722136">
-            <span class="iconify span-icons" data-icon="akar-icons:github-fill"></span>
+             <span class="iconify span-icons" data-icon="akar-icons:linkedin-fill"></span>
         </a>
         <a target="_blank" rel="noreferrer"className='link' href="https://github.com/marthadominguez">
-            <span class="iconify span-icons" data-icon="akar-icons:linkedin-fill"></span>
+           
+            <span class="iconify span-icons" data-icon="akar-icons:github-fill"></span>
         </a>   
         `
     return view;
