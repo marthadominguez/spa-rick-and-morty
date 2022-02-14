f63177b50c5ceaa65c156039fa3b35a08d940a4e
Martha Dominguez <marthadominguezg@gmail.com>
2022-02-14 16:38:48 -0500

    correcciones para despliegue

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,23 @@
-// configuracion que requiere nuestro proyecto
-
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // necesario para trabajar con HTML
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
-    output: { //Donde se envía el proyecto estructurado y compilado listo para producción.
-        path: path.resolve(__dirname, 'dist'), //Creamos el lugar dónde se exportará el proyecto.
-        filename: 'main.js' //Este es el nombre del archivo final para producción.
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
     },
     resolve: {
-        extensions: ['.js']
+        extensions: ['.js'],
     },
     module: {
         rules: [
-            { // Estructura de Babel
-                test: /\.js?$/, //Nos permite identificar los archivos según se encuentran en nuestro entorno.
-                exclude: /node_modules/, //Excluimos la carpeta de node modules
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader' //Utilizar un loader como configuración establecida.
+                    loader: 'babel-loader'
                 }
             }
         ]
@@ -27,15 +25,15 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin(
             {
-                inject: true, //Cómo vamos a inyectar un valor a un archivo HTML.
-                template: './public/index.html',  //Dirección donde se encuentra el template principal
-                filename: './index.html' //El nombre que tendrá el archivo
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html',
             }
-        ),
-        new CopyWebpackPlugin({
-            patterns: [{ from: './src/styles/styles.css',
-            to: '' }],
-      })
+       ),
+       new CopyPlugin({
+        patterns: [
+          { from: './src/styles/styles.css', to: '' },
+        ],
+      }),
     ]
 }
-
